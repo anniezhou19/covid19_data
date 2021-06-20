23c76f62d2d3afacf7436dcefcaf4ef4be8ee9fa
Anurag choudhary <anurag.jay1002@gmail.com>
2021-06-20 12:04:48 +0530

    fixed all bugs (#6)

--- helper.py
+++ helper.py
@@ -1,5 +1,4 @@
-from flask import redirect, render_template, request, session
-from functools import wraps
+from flask import  render_template
 
 
 def apology(message, code=400):
@@ -17,15 +16,3 @@ def apology(message, code=400):
     return render_template("apology.html", top=code, bottom=escape(message)), code
 
 
-def login_required(f):
-    """
-    Decorate routes to require login.
-
-    https://flask.palletsprojects.com/en/1.1.x/patterns/viewdecorators/
-    """
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        if session.get("user_id") is None:
-            return redirect("/login")
-        return f(*args, **kwargs)
-    return decorated_function
